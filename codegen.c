865c88a2bde91908e137d85b4d2b72dc60fc6f6e
gettsu <tsuge-daisuke430@g.ecc.u-tokyo.ac.jp>
2022-01-03 19:18:31 +0900

    [add] unary & and *

--- codegen.c
+++ codegen.c
@@ -5,13 +5,20 @@ char *argreg[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
 int labelseq = 0;
 char *funcname;
 
-void gen_lval(Node *node) {
-    if (node->kind != ND_VAR)
-        error_tok(node->tok, "代入の左辺が変数ではありません");
+void gen(Node *node);
 
-    printf("    mov rax, rbp\n");
-    printf("    sub rax, %d\n", node->var->offset);
-    printf("    push rax\n");
+void gen_lval(Node *node) {
+    switch (node->kind) {
+        case ND_VAR:
+            printf("    mov rax, rbp\n");
+            printf("    sub rax, %d\n", node->var->offset);
+            printf("    push rax\n");
+            return;
+        case ND_DEREF:
+            gen(node->lhs);
+            return;
+    }
+    error_tok(node->tok, "代入の左辺が変数ではありません");
 }
 
 void gen(Node *node) {
@@ -116,6 +123,15 @@ void gen(Node *node) {
             printf("    pop rax\n");
             printf("    mov [rax], rdi\n");
             printf("    push rdi\n");
+            return;
+        case ND_ADDR:
+            gen_lval(node->lhs);
+            return;
+        case ND_DEREF:
+            gen(node->lhs);
+            printf("    pop rax\n");
+            printf("    mov rax, [rax]\n");
+            printf("    push rax\n");
             return;
         case ND_BLOCK:
             for (Node *n = node->body; n; n = n->next) gen(n);
